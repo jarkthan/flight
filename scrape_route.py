b367cbd7dcf7e99ba370821bce815b6ae528fe7b
Thanakorn <93521945+jarkthan@users.noreply.github.com>
2023-04-14 18:45:42 +0700

    Add files via upload

--- scrape_route.py
+++ scrape_route.py
@@ -8,12 +8,12 @@ url = 'https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes
 df = pd.read_csv(url, header=None, names=['Airline', 'AirlineID', 'SourceAirport', 'SourceAirportID', 'DestinationAirport', 'DestinationAirportID', 'Codeshare', 'Stops', 'Equipment'])
 
 # Define the connection string and blob information
-connect_str = 'DefaultEndpointsProtocol=https;AccountName=<your-storage-account-name>;AccountKey=<your-storage-account-key>;EndpointSuffix=core.windows.net'
-container_name = '<your-container-name>'
-blob_name = 'routes.csv'
+connectionString = 'DefaultEndpointsProtocol=https;AccountName=dwthanakornrung;AccountKey=k3+ju9o06l6FeVP722RJdumOGmS4Bxha4XNKucGxcZjhtyxh4AUGY/J2zenszJZknVprkqI5lmAO+ASt2uh9qQ==;EndpointSuffix=core.windows.net'
+container_name = 'data'
+blob_name = 'raw_routes.csv'
 
 # Create a blob service client and blob client to interact with the blob
-blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+blob_service_client = BlobServiceClient.from_connection_string(connectionString)
 container_client = blob_service_client.get_container_client(container_name)
 blob_client = container_client.get_blob_client(blob_name)
 
