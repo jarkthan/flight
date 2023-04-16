a38c3f8228491be013f1aabd1d22e0c32059c27a
Jarkthan <thanakorn.jark@gmail.com>
2023-04-16 19:36:08 +0700

    final

--- scrape_plane.py
+++ scrape_plane.py
@@ -8,8 +8,8 @@ url = 'https://raw.githubusercontent.com/jpatokal/openflights/master/data/planes
 df = pd.read_csv(url, header=None, names=['Name', 'IATA', 'ICAO'])
 
 # Define the connection string and blob information
-connectionString = 'DefaultEndpointsProtocol=https;AccountName=dwthanakornrung;AccountKey=k3+ju9o06l6FeVP722RJdumOGmS4Bxha4XNKucGxcZjhtyxh4AUGY/J2zenszJZknVprkqI5lmAO+ASt2uh9qQ==;EndpointSuffix=core.windows.net'
-container_name = 'data'
+connectionString = "DefaultEndpointsProtocol=https;AccountName=dwhproject1and2;AccountKey=LCWpfIAl/xnoVXm9aGFp/VOUzQjW2VYcN8TB4QI29TTGHFetdNAEHwfKDsHawbuva1ag7mBrvDkW+ASteRQCYQ==;EndpointSuffix=core.windows.net"
+container_name = 'project1'
 blob_name = 'raw_plane.csv'
 
 # Create a blob service client and blob client to interact with the blob
